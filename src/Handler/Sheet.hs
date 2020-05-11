6299d756407a78c49fab9e08621c574c209ed1c6
Patricia <schmidtp@oregonstate.edu>
2020-05-11 14:27:46 -0700

    Add form for min, max config for rand numbers

--- src/Handler/Sheet.hs
+++ src/Handler/Sheet.hs
@@ -8,11 +8,38 @@ module Handler.Sheet where
 import Import
 import System.Random
 
+data Options = Options
+    { minVal :: Int
+    , maxVal :: Int
+    }
+    deriving Show
+
 getSheetR :: Handler Html
 getSheetR = do
-    rand <- liftIO getRand
+    (widget, enctype) <- generateFormPost randIntForm
     defaultLayout $ do 
         $(widgetFile "sheet")
 
-getRand :: IO Int
-getRand = randomRIO (1, 20)
+postSheetR :: Handler Html
+postSheetR = do
+    ((result, widget), enctype) <- runFormPost randIntForm
+    case result of 
+        FormSuccess options -> do
+            rand <- liftIO (getRand (minVal options, maxVal options))
+            defaultLayout $ do 
+                $(widgetFile "show-rand")
+        _ -> defaultLayout $ do 
+            $(widgetFile "sheet")
+    
+
+getRand :: (Int, Int) -> IO Int
+getRand minmax = randomRIO minmax
+
+randIntAForm :: AForm Handler Options
+randIntAForm = Options
+    <$> areq intField "Min Value" Nothing
+    <*> areq intField "Max Value" Nothing
+
+randIntForm :: Html -> MForm Handler (FormResult Options, Widget)
+randIntForm = renderTable randIntAForm
+
